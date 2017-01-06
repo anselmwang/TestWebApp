5feec24e5bcdf3b3bd01fc830000eb9c5e360084
Yu Wang <yuwan@microsoft.com>
2017-01-06 03:04:52 +0000

    create an adder app with bootstrap, vue & flask

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -8,34 +8,10 @@ from FlaskWebProject1 import app
 from flask import request
 
 @app.route('/')
-@app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+    return app.send_static_file("index.html")
+    #return "abcdef"
 
 @app.route('/add')
 def add():
