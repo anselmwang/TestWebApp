3d093e1abb89fef8a9b27248f8f61ff9db30af84
Yu Wang <yuwan@microsoft.com>
2017-01-08 08:36:43 +0000

    Revert "add a digit recoginizer'"
    This reverts commit f57590d3fb26b47f7592a28eeb97c192e12bf9d1.

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -2,30 +2,17 @@
 Routes and views for the flask application.
 """
 
+from datetime import datetime
+from flask import render_template
 from FlaskWebProject1 import app
 from flask import request
-from flask import jsonify
-import digitrecCore
 
 @app.route('/')
 def home():
     """Renders the home page."""
     return app.send_static_file("index.html")
-
+    #return "abcdef"
 
 @app.route('/add')
 def add():
     return str(int(request.args.get('a', '0')) +int(request.args.get('b', '0')))
-
-@app.route("/digitrec")
-def digitrec():
-    """Renders the home page."""
-    return app.send_static_file("digitrec.html")
-    
-
-@app.route('/digitrec_predict', methods=['GET', 'POST'])
-def digitrec_predict():
-    sample = request.get_json()
-    return jsonify({"result": digitrecCore.Predict(sample)})
-    
-
